4e1d50b41f2c790cb079f6292eda82aea977c9d4
ZacharyVoceLTU <20731993@students.latrobe.edu.au>
2025-08-20 15:54:05 +1000

    Add project files.

---
+++ Discovr-GroupNumber.cpp
@@ -0,0 +1,12 @@
+﻿// Discovr-GroupNumber.cpp : Defines the entry point for the application.
+//
+
+#include "Discovr-GroupNumber.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}

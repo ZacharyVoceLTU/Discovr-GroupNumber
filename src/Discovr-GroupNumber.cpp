35f3b7fcd8ecaa8e53e2e457ec7361d98b2dce00
Zachary Voce <20731933@students.latrobe.edu.au>
2025-08-31 20:04:30 +1000

    embedded dll cpp now also extract into dll files

--- src/Discovr-GroupNumber.cpp
+++ src/Discovr-GroupNumber.cpp
@@ -8,9 +8,20 @@
 void createNmapFolder(const std::filesystem::path& tempNmapFolder);
 void extractNmapBinary(const std::filesystem::path& outputPath);
 void deleteTempFolders(const std::filesystem::path& tempNmapFolder);
+void extractNmapDlls(const std::filesystem::path& outputPath, const std::string& fileName, unsigned char* data, unsigned int length);
 
 extern unsigned char nmap_exe[];
 extern unsigned int nmap_exe_len;
+
+// Remove when dynamic reimplementation of extractNmapDlls
+extern unsigned char libcrypto_1_1_dll[];
+extern unsigned int libcrypto_1_1_dll_len; 
+extern unsigned char libssh2_dll[];
+extern unsigned int libssh2_dll_len;
+extern unsigned char libssl_1_1_dll[];
+extern unsigned int libssl_1_1_dll_len;
+extern unsigned char zlibwapi_dll[];
+extern unsigned int zlibwapi_dll_len;
 
 int main()
 {
@@ -20,9 +31,15 @@ int main()
 	
 	try {
 		extractNmapBinary(tempNmapFolder.string());
+
 	} catch (const std::exception& e) {
 		std::cerr << e.what() << '\n';
 	}
+
+	extractNmapDlls(tempNmapFolder.string(), "libcrypto-1_1.dll", libcrypto_1_1_dll, libcrypto_1_1_dll_len);
+	extractNmapDlls(tempNmapFolder.string(), "libssh2.dll", libssh2_dll, libssh2_dll_len);
+	extractNmapDlls(tempNmapFolder.string(), "libssl-1_1.dll", libssl_1_1_dll, libssl_1_1_dll_len);
+	extractNmapDlls(tempNmapFolder.string(), "zlibwapi.dll", zlibwapi_dll, zlibwapi_dll_len);
 
 	deleteTempFolders(tempNmapFolder);
 
@@ -49,6 +66,17 @@ void extractNmapBinary(const std::filesystem::path& outputPath) {
 	}
 
 	outFile.write(reinterpret_cast<const char*>(nmap_exe), nmap_exe_len);
+}
+
+// TODO: Make not hardcoded -> generate list of embedded DLLs in CMake, throw in header to loop over
+void extractNmapDlls(const std::filesystem::path& outputPath, const std::string& fileName, unsigned char* data, unsigned int length) {
+	std::ofstream outFile(outputPath / fileName, std::ios::binary);
+
+	if (!outFile) {
+		throw std::runtime_error("Failed to open file " + fileName + " for writing: " + outputPath.string());
+	}
+
+	outFile.write(reinterpret_cast<const char*>(data), length);
 }
 
 void deleteTempFolders(const std::filesystem::path& tempNmapFolder) {

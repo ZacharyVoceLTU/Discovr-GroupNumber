ace148cb74d117545a8f8f1fce29425f6ed73d27
ZacharyVoceLTU <20731993@students.latrobe.edu.au>
2025-09-21 05:48:02 -0400

    pointless, removed

--- src/Discovr-GroupNumber.cpp
+++ src/Discovr-GroupNumber.cpp
@@ -28,16 +28,6 @@ int main() {
 
 	extractor->extract(tempNmapFolder);
 
-	#if defined(_DEBUG)
-		#if defined(__clang__) 
-			std::cout << "Compiled with Clang!\n";
-		#elif defined(_MSC_VER)
-			std::cout << "Compiled with MSVC!\n";
-		#else
-			std::cout << "Other compiler\n";
-		#endif
-	#endif
-
 	menu(tempNmapFolder);
 
 	deleteTempFolders(tempNmapFolder);
